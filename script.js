1b328e080e83e91e9e4dfa94a62e3128d81b8536
khwongjeremy-netizen <kh.wong.jeremy@gmail.com>
2026-07-03 11:25:07 -0700

    y6eah

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ function toggleQuote() {
         quoteContainer.classList.add('max-h-0' , 'opacity-0');
     }
 }
+function openLikes() {
+    const dropdown = document.getElementById('likes-dropdown');
+    const content = document.getElementById('likes-content');
+
+    const targetHeight = content.scrollHeight + 16;
+
+    dropdown.style.height = targetHeight + 'px';
+    content.classList.remove('opacity-0');
+    content.classList.add('opacity-100');
+}
+
+function closeLikes() {
+    const dropdown = document.getElementById('likes-dropdown');
+    const content = document.getElementById('likes-content');
+
+
+    dropdown.style.height = '0px';
+    content.classList.remove('opacity-100');
+    content.classList.add('opacity-0');
+
+}
