05ab1b95d3ec2227005d57bf2edaa25f22c05a77
khwongjeremy-netizen <kh.wong.jeremy@gmail.com>
2026-08-20 23:13:20 -0700

    yeah

--- index.js
+++ index.js
@@ -1,10 +1,61 @@
 const contentData = {
-    intro: `<h3 class='font-bold items-center'>Navigate</h3><ul class='mt-4 flex flex-col list-disc list-inside text-left'><li><a href='story.html'>-Story</a></li><li><a href='projects.html'>-Projects</a></li><li><a href='contacts.html'>-Contacts</a></li>`,
-    books: `<h3 class='font-bold items-center'>Books</h3><ul class='mt-4 flex flex-col list-disc list-inside text-left' ><li>-Alex Rider</li><li>-Apple In China</li><li>-Guns Germs and Steel</li><li>-Outliers</li><li>-Refugee</li></ul>`,
-    shows: `<h3 class='font-bold items-center'>Shows</h3><ul class='mt-4 flex flex-col list-disc list-inside text-left'><li>-Blue Eyed Samurai</li><li>Breaking bad</li><li>Trollhunter</li><li>Unstable</li></ul>`,
-    anime: `<h3 class='font-bold items-center'>Animes</h3><ul class='mt-4 flex flex-col list-disc list-inside text-left'><li>-Bleach</li><li>-Attack On Titan</li><li>-Vinland Saga</li><li>-Death Note</li><li>-Chainsawman</li></ul>`,
-    music: `<h3 class='font-bold items-center'>Music</h3><ul class='mt-4 flex flex-col list-disc list-inisde'><li>-Apple Cider by Beabadoobee</li><li>-just a dream by yel</li><li>-lost cause by Isabella Peng</li><li>-Sweet Boy by Malcolm Todd</li><li>-twenytyfour by overtonight</li></ul>`,
-    likes: `<h3 class='font-bold items-center'>Likes</h3><ul class='mt-4 flex flex-col list-disc list-inside text-left'><li>-Coding</li><li>-Football</li><li>-Photography</li><li>-Business</li><li>-STEM</li></ul>`
+    intro: `<h3 class='font-bold justify-center'>Navigate</h3>
+                <div id="line-navigate" class ="mt-4 w-full border-t border-0 justify-center transform border-blue-900 z-0"></div>
+                <ul class='mt-4 flex flex-col list-disc list-inside text-left'>
+                    <li>
+                        <a href='story.html'>Story</a>
+                    </li>
+                    <li>
+                        <a href='projects.html'>Projects</a>
+                    </li>
+                    <li>
+                        <a href='contacts.html'>Contacts</a>
+                    </li>
+                </ul>`,
+    books: `<h3 class='font-bold justify-center'>Books</h3>
+            <div id="line-books" class='mt-4 w-full border-t border-0 justify-center transform border-blue-900 z-0"></div>
+            <ul class='mt-4 flex flex-col list-disc list-inside text-left' >
+                <li>Alex Rider</li>
+                <li>Apple In China</li>
+                <li>Guns Germs and Steel</li>
+                <li>Outliers</li>
+                <li>Refugee</li>
+            </ul>`,
+    shows: `<h3 class='font-bold justify-center'>Shows</h3>
+            <div id='line-shows' class='mt-4 w-full border-t border-0 justify-center transform border-blue-900 z-0'></div>
+            <ul class='mt-4 flex flex-col list-disc list-inside text-left'>
+                <li>Blue Eyed Samurai</li>
+                <li>Breaking bad</li>
+                <li>Trollhunter</li>
+                <li>Unstable</li>
+            </ul>`,
+    anime: `<h3 class='font-bold justify-center'>Animes</h3>
+            <div id='line-animes' class='mt-4 w-full border-t border-0 justify-center transform border-blue-900 z-0'></div>
+            <ul class='mt-4 flex flex-col list-disc list-inside text-left'>
+                <li>Bleach</li>
+                <li>Attack On Titan</li>
+                <li>Vinland Saga</li>
+                <li>Death Note</li>
+                <li>Chainsawman</li>
+            </ul>`,
+    music: `<h3 class='font-bold justify-center'>Music</h3>
+            <div id='line-music' class='mt-4 w-full border-t border-0 justify-center transform border-blue-900 z-0'></div>
+            <ul class='mt-4 flex flex-col list-disc list-inside'>
+                <li>Apple Cider by Beabadoobee</li>
+                <li>just a dream by yel</li>
+                <li>lost cause by Isabella Peng</li>
+                <li>Sweet Boy by Malcolm Todd</li>
+                <li>twenytyfour by overtonight</li>
+            </ul>`,
+    likes: `<h3 class='font-bold justify-center'>Likes</h3>
+            <div id='line-music' class='mt-4 w-full border-t border-0 justify-center transform border-blue-900 z-0'></div>
+            <ul class='mt-4 flex flex-col list-disc list-inside text-left'>
+                <li>Coding</li>
+                <li>Football</li>
+                <li>Photography</li>
+                <li>Business</li>
+                <li>STEM</li>
+            </ul>`
 }
 function openDrop(category, type) {
     const master = document.getElementById(`${type}-dropdown`);
