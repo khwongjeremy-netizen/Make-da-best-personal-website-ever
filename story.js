526f24ae737a4eda6ec804b896b0d8f2fd2b3fa1
khwongjeremy-netizen <kh.wong.jeremy@gmail.com>
2026-09-05 12:23:40 -0700

    yeah I am saving this dif

--- story.js
+++ story.js
@@ -74,3 +74,7 @@ function initHoverEffects(card) {
     }
     });
 }
+document.querySelectorAll('.hoverable-text').forEach(el => {
+    const words = el.textContent.trim().split(/\s+/);
+    el.innerHTML = words.map(word => `<span class="inline-block hover:text-blue-400 hover:scale-110 transtion-all duration-300 cursor-d">${word}</span>`)
+})
